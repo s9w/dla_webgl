e5c4db5c965018a93fc565a394de03fba9c8dcb3
Sebastian Werhausen <swerhausen@googlemail.com>
2015-05-26 13:51:52 +0200

    initial commit

---
+++ js/dla.js
@@ -0,0 +1,225 @@
+var stats, scene, camera, renderer, controls;
+var worker = new Worker('js/worker.js');
+var geometry, pointcloud;
+var nParticles = 30000, nextParticleIndex;
+var spawnRadiusLimit = 140;
+var sphere;
+var container;
+var chart;
+var doReset = false;
+var deltaTarget = new THREE.Vector3(0,0,0);
+
+var params = {
+    nParticlesPSec: 10,
+    particleSize: 1.0,
+    displayFog: true,
+    displayBoundary: false,
+    displayDFrac: true,
+    reset: registerReset
+};
+
+function init_gui(container){
+    var gui = new dat.GUI({ autoPlace: false });
+
+    // particles per sec
+    var controller = gui.add(params, "nParticlesPSec", 0, 5000);
+    controller.name("Particles/sec");
+    controller.step(1);
+
+    // particles per sec
+    controller = gui.add(params, "particleSize", 0.2, 2.0);
+    controller.name("Sprite size");
+    controller.onChange(function(value) {
+        pointcloud.material.size = value;
+    });
+
+    // fog toggle
+    controller = gui.add(params, "displayFog");
+    controller.onChange(function(value) {
+        scene.fog.density = value?0.004:0.0;
+    });
+    controller.name("Display fog");
+
+    // boundary toggle
+    controller = gui.add(params, "displayBoundary");
+    controller.onChange(function(value) {
+        sphere.visible = value;
+    });
+    controller.name("Display boundary");
+
+    // Chart toggle
+    controller = gui.add(params, "displayDFrac");
+    controller.onChange(function(value) {
+        document.getElementById('chart').style.visibility = value?'visible':'hidden';
+    });
+    controller.name("Display chart");
+
+    // reset button
+    gui.add(params, "reset");
+
+    gui.domElement.id = 'datGui';
+    container.appendChild(gui.domElement);
+}
+
+function onWindowResize() {
+    camera.aspect = container.clientWidth/container.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( container.clientWidth, container.clientHeight );
+    render();
+}
+
+function registerReset(){
+    doReset = true;
+}
+
+function reset(){
+    doReset = false;
+    for(var i=0; i<nParticles; ++i)
+        pointcloud.geometry.vertices[i].fromArray([99999, 99999, 99999]);
+    pointcloud.geometry.vertices[0].fromArray([0,0,0]);
+    pointcloud.geometry.verticesNeedUpdate = true;
+    nextParticleIndex = 1;
+    chart.series[0].setData([]);
+    params.reset = registerReset;
+    worker.postMessage({ mode: 'init', spawnRadiusLimit: spawnRadiusLimit, nMax: nParticles });
+    worker.postMessage({ mode: '', nParticlesPSec: params.nParticlesPSec });
+}
+
+function init() {
+    container = document.getElementById('glContainer');
+
+    // GUI overlay
+    init_gui(container);
+
+    // renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( container.clientWidth, container.clientHeight );
+    container.appendChild( renderer.domElement );
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    // camera and controls
+    camera = new THREE.PerspectiveCamera( 75, container.clientWidth/container.clientHeight, 0.1, 1000 );
+    camera.position.set(2,4,6);
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.damping = 0.2;
+    controls.addEventListener( 'change', render );
+
+    // scene and boundary
+    scene = new THREE.Scene();
+    scene.fog = new THREE.FogExp2( 0x000000, 0.004 );
+    geometry = new THREE.SphereGeometry( spawnRadiusLimit-10, 16, 16 );
+    material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+    sphere = new THREE.EdgesHelper(  new THREE.Mesh( geometry, material ), 0x0666666 );
+    sphere.visible = params.displayBoundary;
+    scene.add( sphere );
+
+    // init point geometry
+    geometry = new THREE.Geometry();
+    for(var i=0; i<nParticles; ++i){
+        geometry.vertices.push( new THREE.Vector3(99999,99999,99999) );
+        geometry.colors[i] = new THREE.Color();
+        geometry.colors[i].setHSL( i/nParticles, 1, 0.5 );
+    }
+    nextParticleIndex = 1;
+    geometry.vertices[0].set(0,0,0);
+    var sprite = THREE.ImageUtils.loadTexture( "gfx/sphere4.png" );
+    //sprite.generateMipmaps = false;
+    //sprite.minFilter = THREE.NearestFilter;
+    //sprite.magFilter = THREE.NearestFilter;
+    var material = new THREE.PointCloudMaterial( { size:1.1, map: sprite, alphaTest: 0.5, vertexColors: THREE.VertexColors } );
+    pointcloud = new THREE.PointCloud( geometry, material );
+    scene.add( pointcloud );
+
+    // worker
+    worker.onmessage = function(response) {
+        if(doReset)
+            reset();
+        else if(response.data.type=="debug"){
+            console.log(response.data.msg);
+        }
+        else if(!(response.data.goOn)){
+            console.log("end detected.");
+            params.reset = reset;
+        }
+        else{
+            for(var i=0; i< response.data.positions.length; ++i){
+                pointcloud.geometry.vertices[nextParticleIndex].fromArray(response.data.positions[i]);
+                nextParticleIndex++;
+            }
+            pointcloud.geometry.verticesNeedUpdate = true;
+
+            //deltaTarget = response.data.furthestPos;
+
+            var dFrac = Math.log(nextParticleIndex)/Math.log(response.data.maxParticleDistanceS/2);
+            if( dFrac<5 && dFrac>1 && (response.data.positions.length>0)){
+                chart.series[0].addPoint(dFrac);
+                if(chart.series[0].data.length>50)
+                    chart.series[0].removePoint(0);
+            }
+            worker.postMessage({ mode: '', nParticlesPSec: params.nParticlesPSec });
+        }
+    };
+
+    worker.postMessage({
+        mode: 'init',
+        spawnRadiusLimit: spawnRadiusLimit,
+        nMax: nParticles });
+    worker.postMessage({ mode: '', nParticlesPSec: params.nParticlesPSec });
+
+    // chart
+    var chartOptions = {
+        tooltip: { enabled: false },
+        chart: {
+            renderTo: 'chart',
+            type: 'line',
+            backgroundColor: "rgba(255,255,255,0.1)",
+            height: 150,
+            animation: false
+        },
+        plotOptions: {
+            line: {
+                marker: {
+                    enabled: false },
+                states: {
+                    hover: {
+                        enabled: false
+                    } } }
+        },
+        title: {
+            text: 'Fractal Dimension',
+            style: { "color": "#ffffff", "fontSize": "12px" } },
+        xAxis: {
+            labels:{
+                enabled: false } },
+        yAxis: {
+            title: {
+                text: '' },
+            labels:{
+                style: {"color":"#ffffff","fontWeight":"bold"} } },
+        series: [{
+            name: 'dFrac',
+            data: []
+        }],
+        legend: {
+            enabled: false },
+        credits: {
+            enabled: false }
+    };
+    chart = new Highcharts.Chart(chartOptions);
+    animate();
+}
+
+function flyToCenter(){
+    controls.target.x += 0.1;
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+    flyToCenter();
+    controls.update();
+    render();
+}
+
+function render() {
+    renderer.render( scene, camera );
+}
---
+++ js/worker.js
@@ -0,0 +1,128 @@
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/mathjs/1.6.0/math.min.js');
+
+var gridSize, offset;
+var spawnRadius, spawnRadiusLimit, maxParticleDistance;
+var furthestPos = [];
+var position = [];
+var latticeNB=[];
+var t0=-1, t1=-1;
+var nParticles, nParticlesLimit, nParticlesPSec=1;
+var positions = [];
+var countCurrent;
+var updateInterval = 100;
+
+function init(spawnRadiusLimitP){
+    nParticles = 1;
+    maxParticleDistance = 0.0;
+    gridSize = spawnRadiusLimitP*2+1+20;
+    offset = Math.floor(gridSize/2);
+    spawnRadius = maxParticleDistance+10;
+
+    if(latticeNB.length==0)
+        latticeNB = new Array(gridSize * gridSize * gridSize);
+    for (var i = 0; i < latticeNB.length; ++i)
+        latticeNB[i] = 0;
+    latticeNB[offset * gridSize * gridSize + offset * gridSize + (offset + 1)] = 1;
+    latticeNB[offset * gridSize * gridSize + offset * gridSize + (offset - 1)] = 1;
+    latticeNB[offset * gridSize * gridSize + (offset + 1) * gridSize + offset] = 1;
+    latticeNB[offset * gridSize * gridSize + (offset - 1) * gridSize + offset] = 1;
+    latticeNB[(offset + 1) * gridSize * gridSize + offset * gridSize + offset] = 1;
+    latticeNB[(offset - 1) * gridSize * gridSize + offset * gridSize + offset] = 1;
+}
+
+function getStartPos(radius){
+    position[0] = math.random(-radius, radius);
+    position[1] = math.random(-radius, radius);
+    position[2] = math.random(-radius, radius);
+    while((position[0]*position[0] + position[1]*position[1] + position[2]*position[2]) > (radius*radius)){
+        position[0] = math.random(-radius, radius);
+        position[1] = math.random(-radius, radius);
+        position[2] = math.random(-radius, radius);
+    }
+    var length = Math.sqrt(position[0]*position[0] + position[1]*position[1] + position[2]*position[2]);
+    position[0] = Math.floor(position[0]/length*radius);
+    position[1] = Math.floor(position[1]/length*radius);
+    position[2] = Math.floor(position[2]/length*radius);
+    return position;
+}
+
+function getDockPos(){
+    // start somewhere on a sphere from the origin
+    position = getStartPos(spawnRadius);
+
+    // walk until on neighbor site
+    while(latticeNB[(position[0]+offset)*gridSize*gridSize + (position[1]+offset)*gridSize + (position[2]+offset)] == 0){
+        var directionChoice = math.randomInt(3);
+
+        // one step into any direction
+        if(directionChoice == 0)
+            position[0] += math.randomInt(2)*2-1;
+        else if(directionChoice == 1)
+            position[1] += math.randomInt(2)*2-1;
+        else
+            position[2] += math.randomInt(2)*2-1;
+
+        // run out of sphere? reset
+        if( (position[0]*position[0] + position[1]*position[1] + position[2]*position[2]) > (spawnRadius*spawnRadius) )
+            position = getStartPos(spawnRadius);
+    }
+
+    // mark neighbor spots of found position
+    latticeNB[(position[0]+offset  )*gridSize*gridSize + (position[1]+offset  )*gridSize + (position[2]+offset+1)] = 1;
+    latticeNB[(position[0]+offset  )*gridSize*gridSize + (position[1]+offset  )*gridSize + (position[2]+offset-1)] = 1;
+    latticeNB[(position[0]+offset  )*gridSize*gridSize + (position[1]+offset+1)*gridSize + (position[2]+offset  )] = 1;
+    latticeNB[(position[0]+offset  )*gridSize*gridSize + (position[1]+offset-1)*gridSize + (position[2]+offset  )] = 1;
+    latticeNB[(position[0]+offset+1)*gridSize*gridSize + (position[1]+offset  )*gridSize + (position[2]+offset  )] = 1;
+    latticeNB[(position[0]+offset-1)*gridSize*gridSize + (position[1]+offset  )*gridSize + (position[2]+offset  )] = 1;
+
+    // new biggest distance?
+    var radius2 = position[0]*position[0] + position[1]*position[1] + position[2]*position[2];
+    if( radius2 > (maxParticleDistance*maxParticleDistance) ){
+        maxParticleDistance = Math.sqrt(radius2);
+        spawnRadius = maxParticleDistance+10;
+        furthestPos = position.slice();
+    }
+    return position;
+}
+
+function respond(doMore){
+    self.postMessage({
+        type: 'result',
+        positions: positions,
+        maxParticleDistanceS: maxParticleDistance,
+        goOn: doMore,
+        furthestPos: furthestPos
+    });
+    t0 = performance.now();
+    positions = [];
+}
+
+self.addEventListener('message', function(response) {
+    if(response.data.mode == 'init'){
+        //self.postMessage({ type: "debug", msg: "init" });
+        nParticlesLimit = response.data.nMax;
+        init(response.data.spawnRadiusLimit);
+        spawnRadiusLimit = response.data.spawnRadiusLimit;
+        t0 = performance.now();
+    }
+    else{
+        //self.postMessage({ type: "debug", msg: "test" });
+        t0 = performance.now();
+        t1 = t0;
+        countCurrent = 0;
+        nParticlesPSec = response.data.nParticlesPSec;
+        var goOn = true;
+        while(!((t1-t0) > updateInterval || !(goOn) || countCurrent>=(nParticlesPSec/(1000/updateInterval)))){
+            positions.push(getDockPos(spawnRadius).slice());
+            nParticles++;
+            countCurrent++;
+            t1 = performance.now();
+            goOn = !(spawnRadius>spawnRadiusLimit || nParticles>=nParticlesLimit);
+        }
+
+        if((t1-t0) > updateInterval)
+            respond(goOn);
+        else
+            setTimeout(function() { respond(goOn); }, updateInterval);
+    }
+});
